55f2b7aa36b6e4e21fa9ceca5dab7780fb9fdc9b
James Yang <ycyang218@gmail.com>
2020-04-19 01:17:02 -0400

    Updates

---
+++ precache-manifest.eb3fe09a5f1d2afc77036cd193d2facc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc1e12678e9dad6bf5a97b89d801ca4",
+    "url": "/marmalade.fm/index.html"
+  },
+  {
+    "revision": "04ae9f421ffce4ac958c",
+    "url": "/marmalade.fm/static/css/2.2dc38268.chunk.css"
+  },
+  {
+    "revision": "1ef29102812450e5c844",
+    "url": "/marmalade.fm/static/css/main.3cec886b.chunk.css"
+  },
+  {
+    "revision": "04ae9f421ffce4ac958c",
+    "url": "/marmalade.fm/static/js/2.b5eb1cab.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/marmalade.fm/static/js/2.b5eb1cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef29102812450e5c844",
+    "url": "/marmalade.fm/static/js/main.4f025e69.chunk.js"
+  },
+  {
+    "revision": "13d9988bae613bfc16eb",
+    "url": "/marmalade.fm/static/js/runtime-main.1f16cc0c.js"
+  },
+  {
+    "revision": "24cb62684c9357f114b553bca425155f",
+    "url": "/marmalade.fm/static/media/featured-mix.24cb6268.jpg"
+  },
+  {
+    "revision": "0b5c88491bd7b765c17b65c604fccf69",
+    "url": "/marmalade.fm/static/media/pause-button.0b5c8849.svg"
+  },
+  {
+    "revision": "85d7e9aa72b1307322f3bf85dac5d6f0",
+    "url": "/marmalade.fm/static/media/play-button.85d7e9aa.svg"
+  }
+]);
