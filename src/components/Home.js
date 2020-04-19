5eec727cd349eb976d774fef26316d49fbf44f2c
James Yang <ycyang218@gmail.com>
2020-04-18 22:50:17 -0400

    Play buttom is working

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,12 @@ import Mix from './Mix'
 const Home = ({mixes, ...props}) => {
     return (
         <div className = 'flex flex-wrap justify-between mixes ph3 ph4-l'>
-
-        {mixes.map(mix => (
-            <div className = 'mix mb4'>
-            {/* props come from App Component, pass them to Mix component */}
-            <Mix {...props} {...mix}/>
-            </div>
-        ))}
+            {mixes.map(mix => (
+                <div className = 'mix mb4'>
+                {/* props come from App Component, pass them to Mix component */}
+                <Mix {...props} {...mix} id = {mix.key}/>
+                </div>
+            ))}
         </div>
     )
 }
